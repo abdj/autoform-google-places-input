7923c0dfd661d6164543c416db9f427cd1653953
JVercout <jerome.vercoutere@gmail.com>
2015-04-16 18:42:54 +0200

    #feature short_name long_name and #fix remove schema on editing

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'abdj:autoform-google-places-input',
-  version: '0.0.4',
+  version: '0.0.5',
   summary: 'Quick geocomplete input field setup with an export to a customizable address SimpleSchema',
   git: 'https://github.com/abdj/autoform-google-places-input',
   documentation: 'README.md'
@@ -10,11 +10,9 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0.3.2');
   api.use([
     'templating', 
-    'aldeed:autoform@4.2.2', 
-    'dburles:google-maps@1.0.8'
+    'aldeed:autoform', 
+    'dburles:google-maps'
   ], 'client');
-
-  api.imply('dburles:google-maps@1.0.8');
 
   api.addFiles([
     'lib/geocomplete/jquery.geocomplete.js',
